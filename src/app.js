a9f0e5061f7b2ee872182fecf4c9141921e88c0d
kemancipator <kevine@wharton.upenn.edu>
2014-09-13 15:19:34 -0400

    Added array method

--- src/app.js
+++ src/app.js
@@ -1,63 +1,74 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'HandKey',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  subtitle: 'Enter Password',
+  body: 'Press Up/Down'
 });
 
 main.show();
 
+var auth = new Array (0, 1, 0, 1, 0, 1);
+      
+var input = [];
+
 main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  sendMessage();
-  menu.show();
+
+if (auth.length === 6)
+  {checkauthentication(auth,input);} 
+  else {input.push(0);}
+
 });
 
 main.on('click', 'select', function(e) {
+
   var wind = new UI.Window();
   var textfield = new UI.Text({
     position: new Vector2(0, 50),
     size: new Vector2(144, 30),
     font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
+    text: 'HandKey Directions',
     textAlign: 'center'
   });
   wind.add(textfield);
   wind.show();
+
 });
 
 main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  
+  if (auth.length === 6)
+  {checkauthentication(auth,input);}
+  else {input.push(1);}
+  
 });
 
-function sendMessage() {
-  Pebble.sendAppMessage({"key": "JUSTIN"});
+
+ function checkauthentication(a, b)
+
+{if (!Array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (auth.length === input.length)
+    return true;
+ 
+ if (true)
+ {var wind = new UI.Window();
+  var textfield = new UI.Text({
+    position: new Vector2(0, 50),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'HandKey Directions',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
 }
+}
+//{sendMessage();}
+
+//function sendMessage() {
+  //Pebble.sendAppMessage({"key": "Kevin"});
