eb4932aadb63443f9ab2c8807ace59cd724d4f10
jusjmkim <jusjmkim@users.noreply.github.com>
2014-09-13 21:39:56 -0400

    Refactor into functions

--- src/app.js
+++ src/app.js
@@ -1,80 +1,114 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'HandKey',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Enter Password',
-  body: 'Press Up/Down'
-});
-
-main.show();
-
+var main, input;
 var auth = [0, 1, 0, 1, 0, 1];
-      
-var input = [];
 
-main.on('click', 'up', function(e) {
-
-if (input.length >= 6)
-  {checkauthentication(auth,input);} 
-  else {input.push(0);}
-
-});
-
-main.on('click', 'select', function(e) {
-
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'HandKey Directions',
-    textAlign: 'center'
+function createFirstPage() {
+  main = new UI.Card({
+    title: 'HandKey',
+    icon: 'images/menu_icon.png',
+    subtitle: 'Enter Password',
+    body: 'Press Up/Down'
   });
-  wind.add(textfield);
-  wind.show();
-
-});
-
-main.on('click', 'down', function(e) {
   
-  if (input.length >= 6)
-  {checkauthentication(auth,input);}
-  else {input.push(1);}
+  input = [];
+  main.show();
   
-});
+  callListeners(); 
+}
 
+function createTryAgainPage() {
+  var tryAgain = new UI.Card({
+    title: 'Try Again'
+  });
+  
+  tryAgain.show();
+}
 
- function checkauthentication(a, b)
+function upListener() {
+  main.on('click', 'up', function(e) {
+  
+    if (input.length === 6) {
+      input.push(0);
+      checkAuthentication(auth,input);
+    } else {
+      input.push(0);
+    }
+  
+  }); 
+}
 
-{if (!Array)
-        return false;
+function selectListener() {
+  main.on('click', 'select', function(e) {
+  
+    var wind = new UI.Window();
+    var textfield = new UI.Text({
+      position: new Vector2(0, 50),
+      size: new Vector2(144, 30),
+      font: 'gothic-24-bold',
+      text: 'HandKey Directions',
+      textAlign: 'center'
+    });
+    wind.add(textfield);
+    wind.show();
+  
+  }); 
+}
 
-    // compare lengths - can save a lot of time 
-   // if (auth.size !== input.length)
-    // return false;
- var counter = true;
- for (var i=0; i<auth.length; i++){
-   if ((auth[i] !== input[i])) {
-     counter = false;
-     break;
-   }
- }
+function downListener() {
+  main.on('click', 'down', function(e) {
+    
+    if (input.length === 6) {
+      input.push(1);
+      checkAuthentication(auth,input);
+    } else {
+      input.push(1);
+    }
+    
+  });
+}
+
+function callListeners() {
+  upListener();
+  selectListener();
+  downListener();
+}
+
+function checkAuthentication(a, b) {
+  if (!Array) {return false;}
+
+  var counter = true;
+  for (var i = 0; i < auth.length; i++){
+    if (auth[i] !== input[i]) {
+      counter = false;
+      break;
+    }
+  }
  
- if (counter)
- {var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Success!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
+   if (counter) {
+     var wind = new UI.Window();
+     var textfield = new UI.Text({
+       position: new Vector2(0, 50),
+       size: new Vector2(144, 30),
+       font: 'gothic-24-bold',
+       text: 'Success!',
+       textAlign: 'center'
+     });
+     wind.add(textfield);
+     wind.show();
+  } else {
+    createTryAgainPage();
+    setTimeout(function() {
+      createFirstPage();
+    }, 2000);
+  }
 }
-}
+
+(function() {
+  createFirstPage();
+})();
+
 //{sendMessage();}
 
 //function sendMessage() {
